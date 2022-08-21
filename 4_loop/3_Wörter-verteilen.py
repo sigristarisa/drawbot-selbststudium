571234e682eb4a7617f198868fcd8644943e0cca
Coding for the Arts <88927553+cas-c4ta@users.noreply.github.com>
2022-08-21 20:53:42 +0200

    fix white space and var name

--- 4_loop/3_Wörter-verteilen.py
+++ 4_loop/3_Wörter-verteilen.py
@@ -2,10 +2,10 @@
 Wörter verteilen
 """
 
-size(300,300)
-Lieblingsfilme = ["Titanic", "Wild Tales"]
+size(300, 300)
+lieblingsfilme = ["Titanic", "Wild Tales"]
 
-for film in Lieblingsfilme:
+for film in lieblingsfilme:
     text(film, (100, 200))
 
     
@@ -14,7 +14,7 @@ for film in Lieblingsfilme:
 """
     
 Aufgabe: 
-    - Ergänze die Liste Lieblingsfilme mit Filmen, die du gern magst. 
+    - Ergänze die Liste Lieblingsfilme mit Filmen, die du gerne magst. 
     
 Warum werden mit oben stehendem Programm alle Titel übereinander platziert? 
 Kannst du die Filmnamen zufällig auf der Fläche verteilen? 
